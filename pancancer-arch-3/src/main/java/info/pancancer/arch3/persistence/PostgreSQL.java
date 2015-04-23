0443da5a71e3b16c8aff8f79dbef291e61840a69
Brian O'Connor <briandoconnor@gmail.com>
2015-04-23 11:05:43 -0400

    Adding some placeholder methods

--- pancancer-arch-3/src/main/java/info/pancancer/arch3/persistence/PostgreSQL.java
+++ pancancer-arch-3/src/main/java/info/pancancer/arch3/persistence/PostgreSQL.java
@@ -1,7 +1,28 @@
 package info.pancancer.arch3.persistence;
 
+import info.pancancer.arch3.beans.Job;
+import info.pancancer.arch3.beans.Order;
+import info.pancancer.arch3.beans.Provision;
+
 /**
  * Created by boconnor on 2015-04-22.
  */
 public class PostgreSQL {
+
+    public void connect () {
+        
+    }
+
+    public String createOrder (Order order) {
+        return("");
+    }
+
+    public String createJob (Job job) {
+        return("");
+    }
+
+    public String createProvision (Provision provision) {
+        return("");
+    }
+
 }
