0a98d51ddc27a5efafb7ec8f91757e9dad244ca6
Denis Yuen <dyuen@oicr.on.ca>
2015-05-26 17:57:17 -0400

    Run from ubuntu home directory

--- pancancer-arch-3/scripts/run_worker_daemon.sh
+++ pancancer-arch-3/scripts/run_worker_daemon.sh
@@ -3,6 +3,8 @@
 LOG_FILE=/var/log/arch3_worker.log
 PID_FILE=/var/run/arch3_worker.pid
 
+cd ~ubuntu
+
 if [ -f $PID_FILE ]; then
   echo "Existing PID file found at $PID_FILE"
   echo "Maybe kill the Worker daemon (sudo bash kill_worker_daemon.sh) before trying to start it?"
