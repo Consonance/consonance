3fd92c7f5be13c820f650225b408704b7e2209c0
Denis Yuen <denis.yuen@oicr.on.ca>
2015-09-28 16:08:33 -0400

    Work in progress for sharing

--- consonance-client/src/main/java/io/consonance/client/WebClient.java
+++ consonance-client/src/main/java/io/consonance/client/WebClient.java
@@ -1,11 +1,15 @@
 package io.consonance.client;
 
 import com.google.common.base.Joiner;
+import io.consonance.common.ITUtilities;
 import io.swagger.client.ApiClient;
 import io.swagger.client.ApiException;
 import io.swagger.client.api.JobApi;
 import io.swagger.client.model.Job;
+import org.apache.commons.configuration.HierarchicalINIConfiguration;
+import org.apache.commons.io.FileUtils;
 
+import java.io.File;
 import java.util.List;
 
 /**
@@ -17,6 +21,9 @@ public class WebClient extends ApiClient {
     }
 
     public static void main(String args[]) throws ApiException {
+        File configFile = FileUtils.getFile("src", "test", "resources", "config");
+        HierarchicalINIConfiguration parseConfig = ITUtilities.parseConfig(configFile.getAbsolutePath());
+
         WebClient client = new WebClient();
         client.setBasePath("http://localhost:8080");
         client.addDefaultHeader("Authorization", "Bearer 8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918");
@@ -24,5 +31,6 @@ public class WebClient extends ApiClient {
         final List<Job> jobs = jobApi.listOwnedWorkflowRuns();
         Joiner joiner = Joiner.on("; ").skipNulls();
         System.out.println(joiner.join(jobs));
+
     }
 }
---
+++ consonance-webservice/src/test/java/core/UserTest.java
@@ -0,0 +1,45 @@
+package core;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.consonance.webservice.core.ConsonanceUser;
+import io.dropwizard.jackson.Jackson;
+import org.junit.Test;
+
+import java.sql.Timestamp;
+
+import static io.dropwizard.testing.FixtureHelpers.fixture;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author dyuen
+ */
+public class UserTest {
+        private static final ObjectMapper MAPPER = Jackson.newObjectMapper();
+
+        @Test
+        public void serializesToJSON() throws Exception {
+                final ConsonanceUser user = getUser();
+                System.out.println(MAPPER.writeValueAsString(user));
+                final String expected = MAPPER.writeValueAsString(
+                        MAPPER.readValue(fixture("fixtures/user.json"), ConsonanceUser.class));
+                assertThat(MAPPER.writeValueAsString(user)).isEqualTo(expected);
+        }
+
+        @Test
+        public void deserializesFromJSON() throws Exception {
+                final ConsonanceUser user = getUser();
+                assertThat(MAPPER.readValue(fixture("fixtures/user.json"), ConsonanceUser.class))
+                        .isEqualTo(user);
+        }
+
+        private ConsonanceUser getUser() {
+                final ConsonanceUser user = new ConsonanceUser();
+                user.setName("funky user");
+                user.setHashedPassword("password");
+                user.setCreateTimestamp(new Timestamp(0));
+                user.setUpdateTimestamp(new Timestamp(0));
+                user.setUserID(0);
+
+                return user;
+        }
+}
