032a3cb80efc6ecefe2777fcfdd3dbc7f734a78c
Denis Yuen <denis.yuen@oicr.on.ca>
2015-09-29 17:37:27 -0400

    Steal the seqware script and Java interpreter
    (If you can't steal from yourself, who can you steal from?)

---
+++ consonance-client/src/main/java/io/consonance/client/cli/Main.java
@@ -0,0 +1,312 @@
+package io.consonance.client.cli;
+
+import com.google.common.collect.ObjectArrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/*
+ * Based on the SeqWare command.line.
+ */
+public class Main {
+
+    /**
+     * Take a List and create a 'delim' delimited String.
+     *
+     * @param tokens
+     * @param delim
+     * @return
+     */
+    private static String dl(List<String> tokens, String delim) {
+        if (tokens.isEmpty()) {
+            return "";
+        } else {
+            StringBuilder sb = new StringBuilder(tokens.get(0));
+            for (int i = 1; i < tokens.size(); i++) {
+                sb.append(delim);
+                sb.append(tokens.get(i));
+            }
+            return sb.toString();
+        }
+    }
+
+    /**
+     * Create a comma delimited string from a List
+     *
+     * @param tokens
+     * @return
+     */
+    private static String cdl(List<String> tokens) {
+        return dl(tokens, ",");
+    }
+
+    private static int swid(String swid) {
+        try {
+            return Integer.parseInt(swid);
+        } catch (NumberFormatException e) {
+            kill("seqware: invalid seqware accession: '" + swid + "'");
+            return 0; // non-reachable
+        }
+    }
+
+    private static void out(String format, Object... args) {
+        System.out.println(String.format(format, args));
+    }
+
+    private static void err(String format, Object... args) {
+        System.err.println(String.format(format, args));
+    }
+
+    private static List<String> processOverrideParams(List<String> override) {
+        List<String> overrideParams = new ArrayList<>();
+        if (!override.isEmpty()) {
+            overrideParams.add("--");
+            for (String entry : override) {
+                String key = entry.substring(0, entry.indexOf('='));
+                String value = entry.substring(entry.indexOf('=') + 1);
+                overrideParams.add("--" + key);
+                overrideParams.add(value);
+            }
+        }
+        return overrideParams;
+    }
+
+    private static class Kill extends RuntimeException {
+    }
+
+    private static void kill(String format, Object... args) {
+        err(format, args);
+        throw new Kill();
+    }
+
+    private static void invalid(String cmd) {
+        kill("consonance: '%s' is not a consonance command. See 'consonance --help'.", cmd);
+    }
+
+    private static void invalid(String cmd, String sub) {
+        kill("consonance: '%s %s' is not a consonance command. See 'consonance %s --help'.", cmd, sub, cmd);
+    }
+
+    private static void extras(List<String> args, String curCommand) {
+        if (args.size() > 0) {
+            kill("consonance: unexpected arguments to '%s': %s", curCommand, dl(args, " "));
+        }
+    }
+
+    private static boolean flag(List<String> args, String flag) {
+        boolean found = false;
+        for (int i = 0; i < args.size(); i++) {
+            if (flag.equals(args.get(i))) {
+                if (found) {
+                    kill("consonance: multiple instances of '%s'.", flag);
+                } else {
+                    found = true;
+                    args.remove(i);
+                }
+            }
+        }
+        return found;
+    }
+
+    private static List<String> optVals(List<String> args, String key) {
+        List<String> vals = new ArrayList<>();
+
+        for (int i = 0; i < args.size();) {
+            String s = args.get(i);
+            if (key.equals(s)) {
+                args.remove(i);
+                if (i < args.size()) {
+                    String val = args.remove(i);
+                    if (!val.startsWith("--")) {
+                        String[] ss = val.split(",");
+                        if (ss.length > 0) {
+                            vals.addAll(Arrays.asList(ss));
+                            continue;
+                        }
+                    }
+                }
+                kill("seqware: missing required argument to '%s'.", key);
+            } else {
+                i++;
+            }
+        }
+
+        return vals;
+    }
+
+    private static List<String> reqVals(List<String> args, String key) {
+        List<String> vals = optVals(args, key);
+
+        if (vals.isEmpty()) {
+            kill("seqware: missing required flag '%s'.", key);
+        }
+
+        return vals;
+    }
+
+    private static String optVal(List<String> args, String key, String defaultVal) {
+        String val = defaultVal;
+
+        List<String> vals = optVals(args, key);
+        if (vals.size() == 1) {
+            val = vals.get(0);
+        } else if (vals.size() > 1) {
+            kill("consonance: multiple instances of '%s'.", key);
+        }
+
+        return val;
+    }
+
+    private static String reqVal(List<String> args, String key) {
+        String val = optVal(args, key, null);
+
+        if (val == null) {
+            kill("consonance: missing required flag '%s'.", key);
+        }
+
+        return val;
+    }
+
+    private static boolean isHelp(List<String> args, boolean valOnEmpty) {
+        if (args.isEmpty()) {
+            return valOnEmpty;
+        }
+
+        String first = args.get(0);
+        return first.equals("-h") || first.equals("--help");
+    }
+
+    public static final AtomicBoolean DEBUG = new AtomicBoolean(false);
+    public static final AtomicBoolean VERBOSE = new AtomicBoolean(false);
+
+    private static void run(String... args) {
+        if (VERBOSE.get()) {
+            args = ObjectArrays.concat("--verbose", args);
+        }
+        if (DEBUG.get()) {
+            for (int i = 0; i < args.length; i++) {
+                if (args[i].contains(" ")) {
+                    args[i] = "'" + args[i] + "'";
+                }
+            }
+            out("PluginRunner.main: %s", dl(Arrays.asList(args), " "));
+        } else {
+            System.out.println("Do it");
+            //PluginRunner.main(args);
+        }
+    }
+
+    private static void run(List<String> runnerArgs) {
+        run(runnerArgs.toArray(new String[runnerArgs.size()]));
+    }
+
+    // COMMANDS:
+
+
+    /**
+     * Prints to the console without applying any formatting. Useful for situations where output contains unintended formatting strings,
+     * which would break the {@link #out(String format, Object... args)} function. For example, if you try to print an INI file containing
+     * the line "refExclude=XX,GL%,hs37d5,XX_001234" the <i>substring</i> "%,h" will cause String.format to throw an exception and fail. So
+     * it is sometimes necessary to print output with no consideration to formatting.
+     *
+     * @param output
+     */
+    private static void outWithoutFormatting(String output) {
+        System.out.println(output);
+    }
+
+
+    private static void runJob(List<String> args) {
+        if (isHelp(args, true)) {
+            out("");
+            out("Usage: seqware workflow-run --help");
+            out("       seqware workflow-run <sub-command> [--help]");
+            out("");
+            out("Description:");
+            out("  Interact with workflow runs.");
+            out("");
+            out("Sub-commands:");
+            out("  cancel              Cancel a submitted or running workflow run");
+            out("  launch-scheduled    Launch scheduled workflow runs");
+            out("  propagate-statuses  Propagate workflow engine statuses to seqware meta DB");
+            out("  retry               Retry a failed or cancelled workflow run skipping completed steps");
+            out("  reschedule          Reschedule a workflow-run to re-run from scratch as a new run");
+            out("  stderr              Obtain the stderr output of the run");
+            out("  stdout              Obtain the stdout output of the run");
+            out("  report              The details of a given workflow-run");
+            out("  watch               Watch a workflow-run in progress");
+            out("  ini                 Output the effective ini for a workflow run");
+            out("  delete              Recursively delete workflow-runs");
+            out("");
+        } else {
+            String cmd = args.remove(0);
+            if (null != cmd) {
+                switch (cmd) {
+
+                default:
+                    invalid("run", cmd);
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] argv) {
+        List<String> args = new ArrayList<>(Arrays.asList(argv));
+        if (flag(args, "--debug")) {
+            DEBUG.set(true);
+        }
+        if (flag(args, "--verbose")) {
+            VERBOSE.set(true);
+        }
+
+        if (isHelp(args, true)) {
+            out("");
+            out("Usage: consonance [<flag>]");
+            out("       consonance <command> [--help]");
+            out("");
+            out("Commands:");
+            out("  run           Schedule a job");
+            out("  status        Get the status of a job");
+            out("  update        Update this tool to a newer version");
+            // out("  dev           Advanced commands that are useful for developers or debugging");
+            out("");
+            out("Flags:");
+            out("  --help        Print help out");
+            // handled in seqware script:
+            out("  --version     Print Consonance's version");
+            out("  --metadata    Print metadata environment");
+            out("");
+        } else {
+            try {
+                String cmd = args.remove(0);
+                if (null != cmd) {
+                    switch (cmd) {
+                    case "-v":
+                    case "--version":
+                        kill("seqware: version information is provided by the wrapper script.");
+                        break;
+                    case "--metadata":
+                        break;
+                    case "status":
+                        //query(args);
+                        break;
+                    case "update":
+                        //bundle(args);
+                        break;
+                    case "run":
+                        runJob(args);
+                        break;
+                    default:
+                        invalid(cmd);
+                        break;
+                    }
+                }
+            } catch (Kill k) {
+                System.exit(1);
+            }
+        }
+    }
+}
