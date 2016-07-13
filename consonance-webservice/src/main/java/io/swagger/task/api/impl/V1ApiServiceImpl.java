4f406bc109055947b43f196d1013930c7389c4b8
Denis Yuen <denis.yuen@oicr.on.ca>
2016-07-13 11:24:11 -0400

    Checking in work-in-progress

--- consonance-webservice/src/main/java/io/swagger/task/api/impl/V1ApiServiceImpl.java
+++ consonance-webservice/src/main/java/io/swagger/task/api/impl/V1ApiServiceImpl.java
@@ -9,9 +9,13 @@ import io.swagger.task.api.V1ApiService;
 import io.swagger.task.model.Ga4ghTaskExecJob;
 import io.swagger.task.model.Ga4ghTaskExecJobId;
 import io.swagger.task.model.Ga4ghTaskExecJobListResponse;
+import io.swagger.task.model.Ga4ghTaskExecJobLog;
+import io.swagger.task.model.Ga4ghTaskExecState;
 import io.swagger.task.model.Ga4ghTaskExecTask;
 
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
 
 @javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2016-07-12T15:19:07.784Z")
 public class V1ApiServiceImpl extends V1ApiService {
@@ -41,6 +45,49 @@ public class V1ApiServiceImpl extends V1ApiService {
     public Response getJob(String value, ConsonanceUser user)
     throws NotFoundException {
         Ga4ghTaskExecJob job = new Ga4ghTaskExecJob();
+        final Job workflowRun = orderResource.getWorkflowRun(user, value);
+        job.setJobId(workflowRun.getUuid());
+        Ga4ghTaskExecJobLog log = new Ga4ghTaskExecJobLog();
+        log.setStdout(workflowRun.getStdout());
+        log.setStderr(workflowRun.getStderr());
+        List<Ga4ghTaskExecJobLog> logs = new ArrayList<>();
+        logs.add(log);
+        job.setLogs(logs);
+        //job.setMetadata();
+        Ga4ghTaskExecState state;
+        switch (workflowRun.getState()) {
+        case START:
+            state = Ga4ghTaskExecState.Queued;
+            break;
+        case PENDING:
+            state = Ga4ghTaskExecState.Queued;
+            break;
+        case RUNNING:
+            state = Ga4ghTaskExecState.Running;
+            break;
+        case SUCCESS:
+            state = Ga4ghTaskExecState.Complete;
+            break;
+        case FAILED:
+            state = Ga4ghTaskExecState.Error;
+            break;
+        case LOST:
+            state = Ga4ghTaskExecState.Unknown;
+            break;
+        default:
+            state = Ga4ghTaskExecState.Unknown;
+        }
+        job.setState(state);
+        Ga4ghTaskExecTask task = new Ga4ghTaskExecTask();
+//        task.setDescription();
+//        task.setDocker();
+//        task.setInputs();
+//        task.setName();
+//        task.setOutputs();
+//        task.setProjectId();
+//        task.setResources();
+//        task.setTaskId();
+        job.setTask(task);
         return Response.ok().entity(job).build();
     }
     @Override
@@ -52,6 +99,19 @@ public class V1ApiServiceImpl extends V1ApiService {
     @Override
     public Response runTask(Ga4ghTaskExecTask body, ConsonanceUser user)
     throws NotFoundException {
+
+//        final String description = body.getDescription();
+//        final List<Ga4ghTaskExecDockerExecutor> docker = body.getDocker();
+//        final List<Ga4ghTaskExecTaskParameter> inputs = body.getInputs();
+//        final String name = body.getName();
+//        final List<Ga4ghTaskExecTaskParameter> outputs = body.getOutputs();
+//        final Ga4ghTaskExecResources resources = body.getResources();
+
+        Job job = new Job();
+        job.setEndUser(user.getName());
+        orderResource.addOrder(user, job);
+
+
         Ga4ghTaskExecJobId id = new Ga4ghTaskExecJobId();
         return Response.ok().entity(id).build();
     }
