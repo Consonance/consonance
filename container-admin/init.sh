6ec059cd772691fc3cacd0f07094ed894f577228
Denis Yuen <denis.yuen@oicr.on.ca>
2016-10-26 16:02:06 -0400

    Start webservice before daemons

--- container-admin/init.sh
+++ container-admin/init.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# run coordinator and containerProvisioner in background
-nohup java -cp consonance-arch-*.jar io.consonance.arch.coordinator.Coordinator --config config --endless > /consonance_logs/coordinator_nohup.out 2>&1&
-nohup java -cp consonance-arch-*.jar io.consonance.arch.containerProvisioner.ContainerProvisionerThreads --config config --endless > /consonance_logs/container_provisioner_nohup.out 2>&1&
+nohup java -jar consonance-webservice-*.jar server web.yml | tee /consonance_logs/webservice.out &
 
-# run consonance webservice in foreground
-java -jar consonance-webservice-*.jar server web.yml | tee /consonance_logs/webservice.out
+sleep 60
+nohup java -cp consonance-arch-*.jar io.consonance.arch.coordinator.Coordinator --config config --endless | tee /consonance_logs/coordinator_nohup.out &
+nohup java -cp consonance-arch-*.jar io.consonance.arch.containerProvisioner.ContainerProvisionerThreads --config config --endless | tee /consonance_logs/container_provisioner_nohup.out &
