19bc6095d4128beeb72614571418061a826157ee
Denis Yuen <denis.yuen@oicr.on.ca>
2016-10-27 11:59:16 -0400

    Make sure container does not exit

--- container-admin/init.sh
+++ container-admin/init.sh
@@ -2,6 +2,6 @@
 
 nohup java -jar consonance-webservice-*.jar server web.yml | tee /consonance_logs/webservice.out &
 
-sleep 60
 nohup java -cp consonance-arch-*.jar io.consonance.arch.coordinator.Coordinator --config config --endless | tee /consonance_logs/coordinator_nohup.out &
-nohup java -cp consonance-arch-*.jar io.consonance.arch.containerProvisioner.ContainerProvisionerThreads --config config --endless | tee /consonance_logs/container_provisioner_nohup.out &
+
+java -cp consonance-arch-*.jar io.consonance.arch.containerProvisioner.ContainerProvisionerThreads --config config --endless | tee /consonance_logs/container_provisioner_nohup.out
